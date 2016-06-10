aa94248e35a06cd4b4e3d52685d8d854ba90ef90
mariannesimon <mariannesimon95@gmail.com>
2016-06-10 10:43:52 +0200

    code Temp

--- Temp.R
+++ Temp.R
@@ -1,13 +1,27 @@
 rm(list=ls())
+library(nlstools)
 
-#Determination de l'influence de la temperature : 
+# Determination de l'influence de la temperature :
 
 data=read.table("temp.txt",h=T)
-sunflowerplot(data$T,data$Act,main="Activité de l'enzyme en fonction
-     de la température",xlab="Température (°C)",ylab="Activité")
-# Température optimale : environ 35 °C
-data
-#Reste a verifier que la loi d'Arrhenius s'applique
-Ea = 8.31*(5-273)*(10-273)/5*log(mean(data$Act[1:3])/mean(data$Act[4:6]))
-logQ10 = Ea*10/(8.31*(5-273)*(15-273))
+Act = (data$Act*4*10^(-3))/(15*13600)*10^6
+# Representation graphique
+sunflowerplot(data$T,Act,main="Activité de l'enzyme en fonction
+     de la température",xlab="Température (°C)",ylab="Activité (µmol/min)",
+              cex.main=1.2, cex.lab=1, cex.sub=1)
+
+# Loi d'Arrhenius
+v = Act[1:21]*10^(-6)/(60*4*10^(-3))
+k = v/(7*10^(-4))
+invT = 1/(8.31*(data$T[1:21]+273))
+plot(invT,log(k),main="ln(k)=f(1/RT)",xlab="1/RT (T en K)",ylab="log(k)")
+lm1 = lm(log(k)~invT)
+par(mfrow=c(2,2))
+plot(lm1)
+summary(lm1)
+abline(lm1,col="red")
+shapiro.test(residuals(lm1))
+
+
+logQ10 = 17110*10/(8.31*(5+273)*(15+273))
 Q10 = exp(logQ10)
